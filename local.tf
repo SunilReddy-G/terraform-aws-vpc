b45d50468bbd344d36c8d2cdb7b69cad096079e9
sunilreddy.g <sunilsmec@gmail.com>
2026-04-06 17:16:29 +0530

    terraform training

--- local.tf
+++ local.tf
@@ -1,21 +1,23 @@
 locals {
-    common_tags = var.project
-    environment = var.environment
-    terraform = "true"
+    common_tags = {
+        Project = var.project
+        Environment = var.environment
+        Terraform = "true"
+    }
+    vpc_final_tags = merge(
+        local.common_tags,
+        {
+            Name = "${var.project}-${var.environment}"
+        },
+        var.vpc_tags
+    )
+    igw_final_tags = merge(
+        local.common_tags,
+        {
+            Name = "${var.project}-${var.environment}"
+        },
+        var.igw_tags
+    )
+    az_names = slice(data.aws_availability_zones.available.names, 0, 2)
+    
 }
-
-vpc_final_tags = merge(
-    local.common_tags,
-    {
-        Name = "${var.project}-${var.environment}"
-    },
-    var.vpc_tags
-)
-
-igw_final_tags = merge(
-    local.common_tags,
-    {
-        Name = "${var.project}-${var.environment}"
-    },
-    var.igw_tags
-)
